428d429e0c672bc4a27b0d5427c800ac31cbbe4a
ej <e.j.hangzhou@icloud.com>
2022-10-01 19:37:42 +0800

    first commit

---
+++ arch/riscv/kernel/main.c
@@ -0,0 +1,11 @@
+#include "defs.h"
+#include "print.h"
+extern int test();
+
+int main() {
+  puts("Hello riscv \n");
+  // Change the number to your stu_id.
+  put_num(21922192);
+  test();
+  return 0;
+}
---
+++ arch/riscv/libs/print.c
@@ -0,0 +1,14 @@
+#include "defs.h"
+extern struct sbiret sbi_call(uint64_t ext, uint64_t fid, uint64_t arg0,
+                              uint64_t arg1, uint64_t arg2, uint64_t arg3,
+                              uint64_t arg4, uint64_t arg5);
+
+int puts(char *str) {
+  // your code
+  return 0;
+}
+
+int put_num(uint64_t n) {
+  // your code
+  return 0;
+}
